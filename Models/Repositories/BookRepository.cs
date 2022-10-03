60bf5bab21e8e08e5a3284c61399b2e330eda9f0
kani maissa <9ani.maissa@gmail.com>
2022-10-03 16:40:55 +0100

    first commit

---
+++ Models/Repositories/BookRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace BookStore.Models.Repositories
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        List<Book> books;
+
+        public BookRepository()
+        {
+            books = new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Title ="c# Programming",
+                    Description ="No Description"
+                },
+                  new Book()
+                {
+                    Id = 2,
+                    Title ="Angular Programming",
+                    Description ="No Data"
+                },
+
+            };
+        }
+
+        public void Add(Book entity)
+        {
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+          var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(int id,Book newBook)
+        {
+            var book= Find(id);
+            book.Title = newBook.Title;
+            book.Description = newBook.Description;
+        }
+    }
+}
---
+++ Models/Repositories/IBookstoreRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.Models.Repositories
+{
+    public interface IBookstoreRepository <TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int id);
+        void Add(TEntity entity);
+        void Update(int Id,TEntity entity);
+        void Delete(int id);
+    }
+}
